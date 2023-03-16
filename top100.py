992fcb76ee08ebec85f49ac78e9271538c2e8c0d
Rafael Martins <rafaelmartins025@outlook.com>
2023-03-16 09:09:41 -0300

    This Python script uses the Spotify API to retrieve the user's top 100 most listened to songs

---
+++ top100.py
@@ -0,0 +1,26 @@
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+
+# Set up the authentication credentials
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id="xx",
+                                               client_secret="yy",
+                                               redirect_uri="https://localhost:8080/callback",
+                                               scope="user-top-read"))
+
+ranges = ['long_term']
+
+for sp_range in ranges:
+    print("range:", sp_range)
+
+    results = sp.current_user_top_artists(time_range=sp_range, limit=100)
+
+    for i, item in enumerate(results['items']):
+        print(i, item['name'])
+    print()
+
+
+# Get the top 100 most played tracks
+results = sp.current_user_top_tracks(limit=100, time_range="long_term")
+# Print the name and artist of each track
+for i, item in enumerate(results['items']):
+    print(f"{i+1}. {item['name']} - {item['artists'][0]['name']}")
